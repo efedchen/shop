0f7b89c30b35fc4a14c73ab538067109d970edf7
jade <everydayfun@ukr.net>
2017-06-22 22:39:06 +0200

    configured spring mvc; fixed app.properties; fixed pom.xml

---
+++ src/com/onlineShop/main/java/config/SpringSecurityInitializer.java
@@ -0,0 +1,10 @@
+package com.onlineShop.main.java.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+
+public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+
+    // Do nothing
+
+}}
---
+++ src/com/onlineShop/main/java/config/WebSecurityConfig.java
@@ -0,0 +1,48 @@
+package com.onlineShop.main.java.config;
+
+import com.onlineShop.main.java.authentication.MyDBAuthenticationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    MyDBAuthenticationService myDBAauthenticationService;
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+
+        // For User in database.
+        auth.userDetailsService(myDBAauthenticationService);
+
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.csrf().disable();
+
+        http.authorizeRequests().antMatchers("/orderList","/order", "/accountInfo")//
+                .access("hasAnyRole('ROLE_EMPLOYEE', 'ROLE_ADMIN')");
+
+        http.authorizeRequests().antMatchers("/product").access("hasRole('ROLE_ADMIN')");
+
+        http.authorizeRequests().and().exceptionHandling().accessDeniedPage("/403");
+
+        http.authorizeRequests().and().formLogin()//
+                .loginProcessingUrl("/j_spring_security_check") // Submit URL
+                .loginPage("/login")//
+                .defaultSuccessUrl("/accountInfo")//
+                .failureUrl("/login?error=true")//
+                .usernameParameter("userName")//
+                .passwordParameter("password")
+                .and().logout().logoutUrl("/logout").logoutSuccessUrl("/");
+
+    }
+}
