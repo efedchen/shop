5503d291f98ea81f52e64b199e6562b49397019c
jade <everydayfun@ukr.net>
2017-06-29 22:28:03 +0200

    Added main methods of the main controller; fixed visibility modifier in method "addProduct" in class "cartInfo"

--- src/com/onlineShop/main/java/controller/MainController.java
+++ src/com/onlineShop/main/java/controller/MainController.java
@@ -1,17 +1,153 @@
 package com.onlineShop.main.java.controller;
 
+import com.onlineShop.main.java.dao.OrderDAO;
+import com.onlineShop.main.java.dao.ProductDAO;
+import com.onlineShop.main.java.entity.Product;
+import com.onlineShop.main.java.model.CartInfo;
+import com.onlineShop.main.java.model.CustomerInfo;
+import com.onlineShop.main.java.model.PaginationResult;
+import com.onlineShop.main.java.model.ProductInfo;
+import com.onlineShop.main.java.util.Utils;
+import com.onlineShop.main.java.validator.CustomerInfoValidator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.transaction.Transactional;
 
 @Controller
+@Transactional
+@EnableWebMvc
 public class MainController {
 
-    @RequestMapping(value = "/hello", method = RequestMethod.GET)
-    public String helloworld(ModelMap model){
-        model.addAttribute("message1", "Hello WORLD!");
-        return "hello";
+    @Autowired
+    private OrderDAO orderDAO;
+
+    @Autowired
+    private ProductDAO productDAO;
+
+    @Autowired
+    private CustomerInfoValidator customerInfoValidator;
+
+    @InitBinder
+    public void myInitBinder(WebDataBinder dataBinder) {
+        Object targetObj = dataBinder.getTarget();
+        if (targetObj == null) {
+            return;
+        }
+        System.out.println("Target= " + targetObj);
+
+        if (targetObj.getClass() == CartInfo.class) {
+        }
+
+        if (targetObj.getClass() == CustomerInfo.class) {
+            dataBinder.setValidator(customerInfoValidator);
+        }
+    }
+
+    @RequestMapping("/403")
+    public String accessDenied() {
+        return "/403";
+    }
+
+    @RequestMapping("/")
+    public String home() {
+        return "index";
+    }
+
+    @RequestMapping("/productList")
+    public String listProductHandler(Model model,
+                                     @RequestParam(value = "name", defaultValue = "") String likeName,
+                                     @RequestParam(value = "page", defaultValue = "1") int page) {
+        final int maxResult = 5;
+        final int maxNavigationPage = 10;
+
+        PaginationResult<ProductInfo> result = productDAO.queryProducts(page, maxResult, maxNavigationPage, likeName);
+
+        model.addAttribute("paginationProducts", result);
+        return "productList";
+    }
+
+    @RequestMapping({"/buyProduct"})
+    public String listProductHandler(HttpServletRequest request, Model model,
+                                     @RequestParam(value = "code", defaultValue = "") String code) {
+
+        Product product = null;
+        if (code != null && code.length() > 0) {
+            product = productDAO.findProduct(code);
+        }
+
+        if (product != null) {
+            CartInfo cartInfo = Utils.getCartInSession(request);
+
+            ProductInfo productInfo = new ProductInfo(product);
+
+            cartInfo.addProduct(productInfo, 1);
+        }
+        return "redirect:/shoppingCart";
+    }
+
+    @RequestMapping("/shoppingCartRemoveProduct")
+    public String removeProductHandler(HttpServletRequest request, Model model,
+                                       @RequestParam(value = "code", defaultValue = "") String code) {
+        Product product = null;
+
+        if (code != null && code.length() > 0) {
+            product = productDAO.findProduct(code);
+        }
+
+        if (product != null) {
+            CartInfo cartInfo = Utils.getCartInSession(request);
+
+            ProductInfo productInfo = new ProductInfo(product);
+
+            cartInfo.removeProduct(productInfo);
+        }
+        return "redirect:/shoppingCart";
+    }
+
+    @RequestMapping(value = {"/shoppingCart"}, method = RequestMethod.POST)
+    public String shoppingCartUpdateQty(HttpServletRequest request, Model model,
+                                        @ModelAttribute("cartForm") CartInfo cartForm) {
+        CartInfo cartInfo = Utils.getCartInSession(request);
+        cartInfo.updateQuantity(cartForm);
+        return "redirect:/shoppingCart";
+    }
+
+    @RequestMapping(value = {"/shoppingCart"}, method = RequestMethod.GET)
+    public String shoppingCartHandler(HttpServletRequest request, Model model){
+        CartInfo cartInfo = Utils.getCartInSession(request);
+
+        model.addAttribute("cartForm", cartInfo);
+        return "shoppingCart";
+    }
+
+    @RequestMapping(value = "/shoppingCartCustomer", method = RequestMethod.POST)
+    public String shoppingCartCustomerService(HttpServletRequest request, Model model,
+                                              @ModelAttribute("customerForm") @Validated CustomerInfo customerForm,
+                                              BindingResult result, final RedirectAttributes redirectAttributes){
+        if (result.hasErrors()) {
+            customerForm.setValid(false);
+
+            return "shoppingCartCustomer";
+        }
+        customerForm.setValid(true);
+        CartInfo cartInfo = Utils.getCartInSession(request);
+
+        cartInfo.setCustomerInfo(customerForm);
+
+        // Redirect to Confirmation page.
+        return "redirect:/shoppingCartConfirmation";
     }
 }
+
+
 
--- src/com/onlineShop/main/java/model/CartInfo.java
+++ src/com/onlineShop/main/java/model/CartInfo.java
@@ -41,7 +41,7 @@ public class CartInfo {
         return null;
     }
 
-    private void addProduct(ProductInfo productInfo, int quantity) {
+    public void addProduct(ProductInfo productInfo, int quantity) {
         CartLineInfo line = this.findLineByCode(productInfo.getCode());
 
         if (line == null) {
