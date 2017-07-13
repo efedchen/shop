b6e81c4acc1d09660f2ba108de2862b81982fdd4
jade <everydayfun@ukr.net>
2017-07-13 20:36:50 +0200

    add implemantation of AdminController, also small fixes of MainController

--- src/com/onlineShop/main/java/controller/MainController.java
+++ src/com/onlineShop/main/java/controller/MainController.java
@@ -11,9 +11,9 @@ import com.onlineShop.main.java.util.Utils;
 import com.onlineShop.main.java.validator.CustomerInfoValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.WebDataBinder;
@@ -23,7 +23,6 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.transaction.Transactional;
 import java.io.IOException;
 
 @Controller
